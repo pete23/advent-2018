fdadea4736287adc145b3677cab77d7816e90002
Pete Windle <git@pete23.com>
2018-12-06 19:44:45 +0000

    optimisations

--- src/advent-5.clj
+++ src/advent-5.clj
@@ -12,23 +12,17 @@
 (defn atol [a] (Long/parseLong a))
 
 (defn different-case-same-letter [left right]
-  (let [lower-left (Character/toLowerCase left)
-        lower-right (Character/toLowerCase right)
-        left-lower (Character/isLowerCase left)
-        right-lower (Character/isLowerCase right)]
-    (and (= lower-left lower-right)
-         (not (= left-lower right-lower)))))
+  (and (= (Character/toLowerCase left) (Character/toLowerCase right))
+       (not (= (Character/isLowerCase left) (Character/isLowerCase right)))))
 
-(defn reactive? [[left right]]
-  (and left right (different-case-same-letter left right)))
-
-(defn react [stack]
-  (if (reactive? (take-last 2 stack))
+(defn react [sb]
+  (let [l (.length sb)]
+    (if (and (> l 1) (different-case-same-letter
     (vec (drop-last 2 stack))
-    stack))
+    str))
 
 (defn process [input]
-  (reduce #(react (conj %1 %2)) [] input))
+  (reduce #(react (.append %1 %2)) (StringBuilder.) input))
 
 (deftest part-one-test
   (is (= "dabCBAcaDA" (apply str (process "dabAcCaCBAcCcaDA")))))
